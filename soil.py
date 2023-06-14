0441ce5cb8c2d9158a944ffab6ad46979d773e58
Richard <richardpires663802@gmail.com>
2023-06-14 17:10:48 -0300

    watering commit

--- soil.py
+++ soil.py
@@ -2,6 +2,8 @@ import pygame
 from pygame.sprite import Group
 from settings import *
 from pytmx.util_pygame import load_pygame
+from support import *
+from random import choice
 
 
 class SoilTile(pygame.sprite.Sprite):
@@ -12,12 +14,22 @@ class SoilTile(pygame.sprite.Sprite):
         self.z = layers["soil"]
 
 
+class WaterTile(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft=pos)
+        self.z = layers["soil water"]
+
+
 class SoilLayer:
     def __init__(self, all_sprites):
         self.all_sprites = all_sprites
         self.soil_sprites = pygame.sprite.Group()
+        self.water_sprites = pygame.sprite.Group()
 
-        self.soil_surf = pygame.image.load("./setup/graphics/soil/o.png")
+        self.soil_surfs = import_folder_dict("./setup/graphics/soil/")
+        self.water_surfs = import_folder("./setup/graphics/soil_water")
 
         self.create_soil_grid()
         self.create_hit_rects()
@@ -55,13 +67,75 @@ class SoilLayer:
                     self.grid[y][x].append("X")
                     self.create_soil_tiles()
 
+    def water(self, target_pos):
+        for soil_sprite in self.soil_sprites.sprites():
+            if soil_sprite.rect.collidepoint(target_pos):
+                x = soil_sprite.rect.x // tile_size
+                y = soil_sprite.rect.y // tile_size
+
+                self.grid[y][x].append("W")
+                pos = soil_sprite.rect.topleft
+                surf = choice(self.water_surfs)
+                WaterTile(pos, surf, [self.all_sprites, self.water_sprites])
+
+    def remove_water(self):
+        for sprite in self.water_sprites.sprites():
+            sprite.kill()
+
+        for row in self.grid:
+            for cell in row:
+                if "W" in cell:
+                    cell.remove("W")
+
     def create_soil_tiles(self):
         self.soil_sprites.empty()
         for index_row, row in enumerate(self.grid):
             for index_col, cell in enumerate(row):
                 if "X" in cell:
+                    t = "X" in self.grid[index_row - 1][index_col]
+                    b = "X" in self.grid[index_row + 1][index_col]
+                    r = "X" in row[index_col + 1]
+                    l = "X" in row[index_col - 1]
+
+                    tile_type = "o"
+
+                    if all((t, r, b, l)):
+                        tile_type = "x"
+
+                    if l and not any((t, r, b)):
+                        tile_type = "r"
+                    if r and not any((t, l, b)):
+                        tile_type = "l"
+                    if r and l and not any((t, b)):
+                        tile_type = "lr"
+
+                    if t and not any((r, l, b)):
+                        tile_type = "b"
+                    if b and not any((r, l, b)):
+                        tile_type = "t"
+                    if b and t and not any((r, l)):
+                        tile_type = "tb"
+
+                    if l and b and not any((t, r)):
+                        tile_type = "tr"
+                    if r and b and not any((t, l)):
+                        tile_type = "tl"
+                    if l and t and not any((b, r)):
+                        tile_type = "br"
+                    if r and t and not any((b, l)):
+                        tile_type = "bl"
+
+                    if all((t, b, r)) and not l:
+                        tile_type = "tbr"
+                    if all((t, b, l)) and not r:
+                        tile_type = "tbl"
+                    if all((l, r, t)) and not b:
+                        tile_type = "lrb"
+                    if all((l, r, b)) and not t:
+                        tile_type = "lrt"
+
                     SoilTile(
                         pos=(index_col * tile_size, index_row * tile_size),
-                        surf=self.soil_surf,
+                        surf=self.soil_surfs[tile_type],
                         groups=[self.all_sprites, self.soil_sprites],
                     )
